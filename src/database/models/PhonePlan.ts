336f71ec9dbd245e59aed4755421b43bceae3f17
Fernando Serpa <serpaef@outlook.com>
2022-04-13 14:45:08 -0300

    feat: Cria models de planos e tarifas

---
+++ src/database/models/PhonePlan.ts
@@ -0,0 +1,33 @@
+import database from '.';
+import { DataTypes, Model } from 'sequelize';
+
+class PhonePlans extends Model {
+  id: number;
+  plan: string;
+  minutes: number;
+}
+
+PhonePlans.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    plan: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    minutes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: database,
+    tableName: 'PhonePlans',
+    timestamps: false,
+  }
+);
+
+export default PhonePlans;
---
+++ src/database/models/Rates.ts
@@ -0,0 +1,39 @@
+import database from '.';
+import { DataTypes, Model } from 'sequelize';
+
+class Rates extends Model {
+  id: number;
+  origin: string;
+  destination: string;
+  pricePerMinute: number;
+}
+
+Rates.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    origin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    destination: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pricePerMinute: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: database,
+    tableName: 'PhonePlans',
+    timestamps: false,
+    underscored: true,
+  }
+);
+
+export default Rates;
