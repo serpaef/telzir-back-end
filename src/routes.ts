ec845dfd5111e89e347c18c7ce44a8c700272a7c
Fernando Serpa <serpaef@outlook.com>
2022-04-13 15:45:29 -0300

    feat: Cria rota para recuperar tarifas.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import PhonePlansController from './controllers/PhonePlansController';
+import RatesController from './controllers/RatesController';
 
 const routes = Router();
 
@@ -7,5 +8,6 @@ routes.get('/ping', (_req, res) => {
   res.status(200).send({message: 'pong'});
 })
 routes.get('/plans', PhonePlansController.getAll);
+routes.get('/rates', RatesController.getAll);
 
 export default routes;
