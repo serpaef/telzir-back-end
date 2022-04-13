0173d24a9b82b07404bc517b3946ac271d6edf46
Fernando Serpa <serpaef@outlook.com>
2022-04-13 15:36:25 -0300

    feat: Cria rota para receber dados dos planos

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import PhonePlansController from './controllers/PhonePlansController';
 
 const routes = Router();
 
 routes.get('/ping', (_req, res) => {
   res.status(200).send({message: 'pong'});
 })
+routes.get('/plans', PhonePlansController.getAll);
 
 export default routes;
