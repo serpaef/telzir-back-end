85aa39200cfdd35d937f21f60104a561ab469beb
Fernando Serpa <serpaef@outlook.com>
2022-04-13 15:19:28 -0300

    feat: Cria service para recuperar todos os planos

---
+++ src/services/PhonePlansService.ts
@@ -0,0 +1,12 @@
+import PhonePlans from '../database/models/PhonePlan';
+
+class PhonePlansService {
+  static async getAll() {
+    try {
+      const plans: PhonePlans[] = await PhonePlans.findAll();
+      return plans
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+}
