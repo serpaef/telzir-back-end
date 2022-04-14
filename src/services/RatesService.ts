30b4a94d2d46c61c331f9b3f6f1b7541747edbde
Fernando Serpa <serpaef@outlook.com>
2022-04-14 10:23:52 -0300

    fix: Corrige tipagem de valor recebido em RatesService

--- src/services/RatesService.ts
+++ src/services/RatesService.ts
@@ -1,9 +1,10 @@
 import Rates from '../database/models/Rates';
+import IRates from '../interfaces/IRates';
 
 class RatesService {
   static async getAll() {
     // I thought it would be better to let the exception be thrown, and handle it in the controller 
-    const plans: Rates[] = await Rates.findAll();
+    const plans: IRates[] = await Rates.findAll();
     return plans;
   }
 }
