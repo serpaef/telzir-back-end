13847cc46d2d8ca24d026c0cdb3c73c820078ded
Fernando Serpa <serpaef@outlook.com>
2022-04-14 10:08:46 -0300

    fix: Corrige tipagem no getAll

--- src/services/PhonePlansService.ts
+++ src/services/PhonePlansService.ts
@@ -1,9 +1,10 @@
 import PhonePlans from '../database/models/PhonePlan';
+import IPhonePlan from '../interfaces/IPhonePlan';
 
 class PhonePlansService {
   static async getAll() {
     // I thought it would be better to let the exception be thrown, and handle it in the controller
-    const plans: PhonePlans[] = await PhonePlans.findAll();
+    const plans: IPhonePlan[] = await PhonePlans.findAll();
     return plans;
   }
 }
