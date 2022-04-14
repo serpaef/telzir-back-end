e28e51db1a1fe9164fd4ffe5ce312a2a612973fb
Fernando Serpa <serpaef@outlook.com>
2022-04-14 10:17:33 -0300

    style: Adequa formato do mock para lint.

--- src/tests/mocks/plans.ts
+++ src/tests/mocks/plans.ts
@@ -1,7 +1,7 @@
 import IPhonePlan from '../../interfaces/IPhonePlan';
 
 export const plans: IPhonePlan[] = [
-  {id:1, plan: 'Fale Mais 30', minutes: 30},
-  {id:2, plan: 'Fale Mais 60', minutes: 60},
-  {id:3, plan: 'Fale Mais 120', minutes: 120},
-]
+  { id: 1, plan: 'Fale Mais 30', minutes: 30 },
+  { id: 2, plan: 'Fale Mais 60', minutes: 60 },
+  { id: 3, plan: 'Fale Mais 120', minutes: 120 },
+];
